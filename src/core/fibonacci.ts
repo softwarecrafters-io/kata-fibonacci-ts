12721bc9d2dc6555e684ca643280a73b3c4e28dc
Miguel A Gomez <miguelghz@Mac-Pro-Software-Crafters.local>
2020-06-13 13:30:23 +0100

    Add recursive case

--- src/core/fibonacci.ts
+++ src/core/fibonacci.ts
@@ -1,5 +1,5 @@
 export const fibonacci = (n: number) => {
 	if (n === 0) return 0;
 	if (n === 1 || n === 2) return 1;
-	else return 2;
+	return fibonacci(n - 1) + fibonacci(n - 2);
 };
--- src/tests/fibonacci.test.ts
+++ src/tests/fibonacci.test.ts
@@ -1,11 +1,12 @@
 import { fibonacci } from '../core/fibonacci';
 
-test('fibonnaci should', () => {
+test('Calculate fibonnaci number', () => {
 	const testCases = [
 		{ value: 0, expected: 0 },
 		{ value: 1, expected: 1 },
 		{ value: 2, expected: 1 },
 		{ value: 3, expected: 2 },
+		{ value: 4, expected: 3 },
 	];
 
 	testCases.forEach((testCase) => expect(fibonacci(testCase.value)).toBe(testCase.expected));
